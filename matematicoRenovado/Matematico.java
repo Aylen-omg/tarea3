5f0cadd2aa7580804e3e9d1e58d3c44251d1cac9
Aylen-omg <82118099+Aylen-omg@users.noreply.github.com>
2021-06-19 23:27:28 -0400

    Update Matematico.java

--- matematicoRenovado/Matematico.java
+++ matematicoRenovado/Matematico.java
@@ -7,20 +7,20 @@ public class Matematico
 
     public String  valoresDeX(int a, int b, int c) {
         String respuesta;
-        respuesta= calculadora.valoresDeX(a, b, c);
+        respuesta = calculadora.valoresDeX(a, b, c);
 
         return respuesta;        
     }
 
-    public String ecuacionRecta( int x1,int  y1,int x2, int y2){
+    public String ecuacionRecta(int x1, int  y1, int x2, int y2){
         String respuesta;
-        respuesta =calculadora.ecuacionRecta(x1,y1,x2,y2);
+        respuesta = calculadora.ecuacionRecta(x1, y1, x2, y2);
         return respuesta;
     }
 
-    public double hallarCateto ( int h, int c1){
+    public double hallarCateto(int h, int c1){
         double respuesta;
-        respuesta= calculadora.hallarCateto(h, c1);
+        respuesta = calculadora.hallarCateto(h, c1);
         return respuesta;
     }   
 }
