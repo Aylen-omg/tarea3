27de8aa6a472f229ba7acee2ad116f23f3f26431
Aylen-omg <82118099+Aylen-omg@users.noreply.github.com>
2021-06-19 23:22:51 -0400

    Update Calculadora.java

--- matematicoRenovado/Calculadora.java
+++ matematicoRenovado/Calculadora.java
@@ -13,20 +13,20 @@ public class Calculadora
 
     }
 
-    public String ecuacionRecta( int x1,int  y1,int x2, int y2){
+    public String ecuacionRecta(int x1, int  y1, int x2, int y2){
         double ce;
         double pendiente;
         String ecuacion;
-        pendiente= (y2-y1)/(x2-x1);
-        ce=  y1-((y2-y1)/(x2-x1))*x1;
+        pendiente = (y2-y1)/(x2-x1);
+        ce =  y1-((y2-y1)/(x2-x1))*x1;
         ecuacion = "y= "+ pendiente+"x"+ "+"+ ce;
 
         return ecuacion;
     }
 
-    public double hallarCateto ( int h, int c1){
+    public double hallarCateto(int h, int c1){
         double c2;
-        c2= (Math.sqrt(Math.pow(h, 2)- Math.pow(c1,2)));
+        c2 = (Math.sqrt(Math.pow(h, 2)- Math.pow(c1,2)));
         return c2;
     }   
 
